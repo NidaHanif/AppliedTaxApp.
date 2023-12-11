dc28e67863c5bd861295804e479f9805ec0fdc6b
Mohammed Amir <aamir_jk@hotmail.com>
2023-12-11 11:31:31 +0300

    Commit Staged by Aamir in Branch Nida

--- AppliedTax/Program.cs
+++ AppliedTax/Program.cs
@@ -1,3 +1,4 @@
+//using Append.Blazor.Printing;
 using AppliedTax.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
@@ -8,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+//builder.Services.AddScoped<IPrintingService, PrintingService>();
 
 var app = builder.Build();
 
